1afd9326b1f4cfb372ad68517e93930eab1fb128
Abdulsomad Olayiwola Abdulwahab <abdulsomad005@gmail.com>
2025-05-28 16:00:12 +0100

    Update settings.py

--- Weather_App_with_Django/settings.py
+++ Weather_App_with_Django/settings.py
@@ -26,7 +26,10 @@ SECRET_KEY = 'django-insecure--9$lv0k^yg9m#@+!^#x=6qi6j_8656wlh1h1$+56zh^-hrfm9w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['*','localhost',
+    '127.0.0.1',
+    '.pipeops.net', # This wildcard allows all subdomains under pipeops.net
+    'redundant-property.pipeops.net']
 
 
 # Application definition
