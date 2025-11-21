97c79a83199fe761d96cb90cef86accd02ad1b72
Abdulsomad Olayiwola Abdulwahab <abdulsomad005@gmail.com>
2025-11-21 14:33:38 +0100

    Set DEBUG to True in settings.py
    Changed DEBUG setting to True for development.

--- Weather_App_with_Django/settings.py
+++ Weather_App_with_Django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--9$lv0k^yg9m#@+!^#x=6qi6j_8656wlh1h1$+56zh^-hrfm9w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
